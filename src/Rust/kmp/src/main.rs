d675a4c2488a8326aae4c7db1112c995d4d1c601
Randy J. Ray <rjray@blackperl.com>
2022-09-19 22:41:37 -0700

    Just testing setup.rs

--- src/Rust/kmp/src/main.rs
+++ src/Rust/kmp/src/main.rs
@@ -1,3 +1,24 @@
+use common::setup::*;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let sequences: Vec<String> = read_sequences(&args[1]);
+    println!("{} sequences read from {}", sequences.len(), &args[1]);
+
+    let patterns: Vec<String> = read_patterns(&args[2]);
+    println!("{} patterns read from {}", patterns.len(), &args[2]);
+
+    if args.len() > 3 {
+        let answers: Vec<Vec<u32>> = read_answers(&args[3]);
+        println!("{} rows of answers read from {}", answers.len(), &args[3]);
+
+        if patterns.len() != answers.len() {
+            println!("Count mismatch between patterns and answers");
+        }
+        if sequences.len() != answers[0].len() {
+            println!("Count mismatch between answers and sequences");
+        }
+    }
 }
