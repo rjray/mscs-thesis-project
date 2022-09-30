6a34b8529a0ab7283301c0fed4669bd0ee214580
Randy J. Ray <rjray@blackperl.com>
2022-09-30 13:26:42 -0700

    Support for running valgrind across the board

--- src/defines.mk
+++ src/defines.mk
@@ -2,6 +2,7 @@
 
 ALGORITHMS := kmp boyer_moore shift_or aho_corasick
 
+# Macros for running the experiments. One for testing and one for real.
 define RUN_test_experiment
 @$(1) $(SEQUENCES) $(PATTERNS) $(ANSWERS)
 
@@ -10,3 +11,23 @@ define RUN_experiment
 @$(HARNESS) -v -n $(RUNCOUNT) -f $(EXPERIMENTS_FILE) $(1) $(SEQUENCES) $(PATTERNS) $(ANSWERS)
 
 endef
+
+# Macro for running an experiment instance under valgrind.
+define RUN_memcheck
+$(VALGRIND) $(1) $(SEQUENCES) $(PATTERNS) $(ANSWERS) 1>/dev/null 2>>$(MEMCHECK_FILE)
+@echo >> $(MEMCHECK_FILE)
+
+endef
+
+# Rule to run memory checking:
+memcheck: $(TARGETS)
+ifeq ($(SEQUENCES),)
+	$(error Sequences file not specified, cannot run memcheck)
+endif
+ifeq ($(PATTERNS),)
+	$(error Patterns file not specified, cannot run memcheck)
+endif
+ifeq ($(ANSWERS),)
+	$(warning Answer file not specified, no checking will be done)
+endif
+	$(foreach target,$(TARGETS),$(call RUN_memcheck,$(target)))
