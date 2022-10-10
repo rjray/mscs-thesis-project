e7001b40889727bd15fa759a96922972fd186ed2
Randy J. Ray <rjray@blackperl.com>
2022-10-10 11:09:32 -0700

    Boyer-Moore implementation for Python

---
+++ src/Python/boyer_moore.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+
+from run import run
+from sys import argv
+
+ASIZE = 128
+
+
+def calc_bad_char(pat, m, bad_char):
+    for i in range(m - 1):
+        bad_char[pat[i]] = m - 1 - i
+
+    return
+
+
+def calc_suffixes(pat, m, suffix_list):
+    suffix_list[m - 1] = m
+
+    f = 0
+    g = m - 1
+    for i in range(m - 2, -1, -1):
+        if i > g and suffix_list[i + m - 1 - f] < i - g:
+            suffix_list[i] = suffix_list[i + m - 1 - f]
+        else:
+            if i < g:
+                g = i
+
+            f = i
+            while g >= 0 and pat[g] == pat[g + m - 1 - f]:
+                g -= 1
+
+            suffix_list[i] = f - g
+
+    return
+
+
+def calc_good_suffix(pat, m, good_suffix):
+    suffixes = [0] * m
+
+    calc_suffixes(pat, m, suffixes)
+
+    j = 0
+    i = m - 1
+    while i >= -1:
+        if i == -1 or suffixes[i] == i + 1:
+            while j < m - 1 - i:
+                if good_suffix[j] == m:
+                    good_suffix[j] = m - 1 - i
+
+                j += 1
+
+        i -= 1
+
+    for i in range(m - 1):
+        good_suffix[m - 1 - suffixes[i]] = m - 1 - i
+
+    return
+
+
+def boyer_moore(pattern, m, sequence, n):
+    pat = list(map(ord, pattern))
+    pat.append(0)
+    seq = list(map(ord, sequence))
+    matches = 0
+
+    good_suffix = [m] * m
+    bad_char = [m] * ASIZE
+
+    calc_good_suffix(pat, m, good_suffix)
+    calc_bad_char(pat, m, bad_char)
+
+    j = 0
+    while j <= n - m:
+        i = m - 1
+        while i >= 0 and pat[i] == seq[i + j]:
+            i -= 1
+        if i < 0:
+            matches += 1
+            j += good_suffix[0]
+        else:
+            j += max(good_suffix[i], bad_char[seq[i + j]] - m + 1 + i)
+
+    return matches
+
+
+if __name__ == "__main__":
+    exit(run(boyer_moore, "boyer_moore", argv))
